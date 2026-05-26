c380d7bbf6ac2104dc1e72d1cf3eea9961bb76c0
jeonyoungho <yhjun1000@gmail.com>
2026-05-26 21:28:53 +0900

    chore: JPA plugin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.jeonyongho.gls"
